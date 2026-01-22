90ad2e643f5b720cedf55406d0a8b15e19d47618
Adriel Sand <sandadriel@outlook.com>
2026-01-22 08:34:27 -0500

    Fix bug where sheet music button still showed with no music; fixes #1

--- app/src/main/java/org/sda/hymnal/MainApplication.kt
+++ app/src/main/java/org/sda/hymnal/MainApplication.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -22,7 +21,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -32,7 +30,6 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import org.sda.hymnal.data.Hymn
@@ -41,14 +38,13 @@ import org.sda.hymnal.data.Hymnals
 import org.sda.hymnal.data.JsonHymn
 import org.sda.hymnal.data.hymnalList
 import org.sda.hymnal.screen.HomeScreen
-import org.sda.hymnal.screen.ListScreen
 import org.sda.hymnal.screen.HymnScreen
 import org.sda.hymnal.screen.HymnalEvent
-import org.sda.hymnal.screen.NavigationScreens
 import org.sda.hymnal.screen.HymnalViewModel
+import org.sda.hymnal.screen.ListScreen
+import org.sda.hymnal.screen.NavigationScreens
 import org.sda.hymnal.screen.Screen
 import org.sda.hymnal.screen.SheetMusicScreen
-import kotlin.collections.mutableListOf
 
 fun loadHymns(context: Context, hymnal: Hymnal): List<Hymn>{
 //    for (hymnal in hymnalList) {
@@ -240,13 +236,22 @@ inline fun <reified S : Screen> NavGraphBuilder.screen(
 @SuppressLint("DiscouragedApi")
 fun getSheetMusic(context: Context, hymn: Hymn): List<Int> {
     val resourceArray = arrayListOf<Int>()
+    if (hymn.hymnal.sheetsPrefix == "") {       // Exit early if there is no sheetsPrefix
+        return resourceArray.toList()
+    }
+    val hymnName = hymn.hymnal.sheetsPrefix + hymn.number.toString().padStart(3,'0')
+    if (hymnName.toIntOrNull() != null) {       // Have to check if hymnName is just integer in which case sheet music doesn't exist
+        return resourceArray.toList()
+    }
     val resource = context.resources.getIdentifier(
-        hymn.hymnal.sheetsPrefix + hymn.number.toString().padStart(3,'0'),
+        hymnName,
         "drawable",
         context.packageName
     )
-    if (resource != 0) {
+    if (resource != 0) {                        // Check if resource exists
         resourceArray.add(resource)
+    } else {
+        return resourceArray.toList()
     }
     for (num in 1..6) {
         val resource = context.resources.getIdentifier(
