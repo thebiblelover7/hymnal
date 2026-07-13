1ea5eb971fa67017b06c06e7c6012652471b13bc
Adriel Sand <sandadriel@outlook.com>
2026-07-13 10:01:45 +0300

    Fixed names of hymnals and added publication dates, fixes #14

--- app/src/main/java/org/sda/hymnal/data/hymnal/Hymnals.kt
+++ app/src/main/java/org/sda/hymnal/data/hymnal/Hymnals.kt
@@ -2,10 +2,10 @@ package org.sda.hymnal.data.hymnal
 
 val hymnalList = listOf(Hymnals.NewEnglish, Hymnals.OldEnglish, Hymnals.NewSpanish, Hymnals.OldSpanish, Hymnals.Portuguese, Hymnals.Russian)
 object Hymnals {
-    object NewEnglish: Hymnal("English - New Hymnal", "new-hymnal-en", "sheets_new_en_")
-    object OldEnglish: Hymnal("English - Old Hymnal", "old-hymnal-en", "")
-    object NewSpanish: Hymnal("Español - Nuevo Himnario", "new-hymnal-es", "sheets_new_es_")
-    object OldSpanish: Hymnal("Español - Viejo Himnario", "old-hymnal-es", "")
-    object Portuguese: Hymnal("Português - Novo Hinario", "new-hymnal-pt", "")
-    object Russian: Hymnal("Russian - Hymnal", "new-hymnal-ru", "sheets_ru_")
+    object NewEnglish: Hymnal("English - New Hymnal (1985)", "new-hymnal-en", "sheets_new_en_")
+    object OldEnglish: Hymnal("English - Old Hymnal (1941)", "old-hymnal-en", "")
+    object NewSpanish: Hymnal("Español - Nuevo Himnario (2010)", "new-hymnal-es", "sheets_new_es_")
+    object OldSpanish: Hymnal("Español - Viejo Himnario (1962)", "old-hymnal-es", "")
+    object Portuguese: Hymnal("Português - Novo Hinario (1996)", "new-hymnal-pt", "")
+    object Russian: Hymnal("Русский - Гимны Надежды", "new-hymnal-ru", "sheets_ru_")
 }
