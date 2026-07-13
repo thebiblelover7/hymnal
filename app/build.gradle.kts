5e8030ec6b4a1c30a17cfd18091eb3dcae942387
Adriel Sand <sandadriel@outlook.com>
2026-07-13 10:06:45 +0300

    Update version code to 6 (v0.2.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.sda.hymnal"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.0"
+        versionCode = 6
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
